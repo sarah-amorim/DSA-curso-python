44c0031555501f2e32dfa8856d6a1bc99b31ebcd
Sarah Amorim <162258709+sarah-amorim@users.noreply.github.com>
2025-06-15 20:32:14 -0300

    Update exercicio04.py

---
+++ modulo-05/loops-condicionais/exercicio04.py
@@ -0,0 +1,9 @@
+# Exercício 4 - Crie uma sequência de números pares entre 100 e 150 e imprima na tela
+
+pares = [x for x in range(100, 150, 2)]
+print(pares)
+
+"""
+for i in range(100, 151, 2):
+    print(i)
+"""

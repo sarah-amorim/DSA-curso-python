63eb38125ee4cc8962b0fd0e85f6a394185d2358
Sarah Amorim <162258709+sarah-amorim@users.noreply.github.com>
2025-06-15 20:26:55 -0300

    Update exercicio02.py

---
+++ modulo-05/loops-condicionais/exercicio02.py
@@ -0,0 +1,14 @@
+# Exercício 2 - Crie uma lista de 5 frutas e verifique se a fruta 'Morango' faz parte da lista
+
+frutas = ["limão", "kiwi", "figo", "morango", "abacaxi"]
+if "morango" in frutas:
+  print("Morango está na lista")
+else:
+  print("Morango não está na lista")
+
+"""
+lista = ['Laranja', 'Maça', 'Abacaxi', 'Uva', 'Morango']
+for fruta in lista:
+    if fruta == 'Morango':
+        print("Morango faz parte da lista de frutas.")
+"""

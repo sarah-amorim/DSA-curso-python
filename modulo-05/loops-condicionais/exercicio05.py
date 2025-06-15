acb15a65ccc6043f22c86ea6e048b71dc4e7dd02
Sarah Amorim <162258709+sarah-amorim@users.noreply.github.com>
2025-06-15 20:35:27 -0300

    Update exercicio05.py

---
+++ modulo-05/loops-condicionais/exercicio05.py
@@ -0,0 +1,14 @@
+# Exercício 5 - Crie uma variável chamada temperatura e atribua o valor 40. Enquanto temperatura for maior que 35, 
+# imprima as temperaturas na tela
+
+temperatura = 40
+while temperatura > 35:
+  print(temperatura)
+  temperatura -= 1
+
+"""
+temperatura = 40  
+while temperatura > 35: 
+    print(temperatura)
+    temperatura = temperatura - 1
+"""

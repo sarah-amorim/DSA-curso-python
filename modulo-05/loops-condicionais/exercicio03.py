fc7b1fb8822a85c7e18533abb4e2b013f99b4acc
Sarah Amorim <162258709+sarah-amorim@users.noreply.github.com>
2025-06-15 20:29:40 -0300

    Update exercicio03.py

---
+++ modulo-05/loops-condicionais/exercicio03.py
@@ -0,0 +1,14 @@
+# Exercício 3 - Crie uma tupla de 4 elementos, multiplique cada elemento da tupla por 2 e guarde os resultados em uma lista
+
+numeros = {1, 2, 3, 4}
+multiplicacao = [x*2 for x in numeros]
+print(multiplicacao)
+
+"""
+tup1 = (1, 2, 3, 4)
+lst1 = []
+for i in tup1:
+    novo_valor = i * 2
+    lst1.append(novo_valor)
+print(lst1)
+"""

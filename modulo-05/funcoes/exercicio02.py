d97568b88280d5af6533ff4214cd2423cef88c27
Sarah Amorim <162258709+sarah-amorim@users.noreply.github.com>
2025-06-15 18:15:24 -0300

    Update exercicio02.py

--- modulo-05/funcoes/exercicio02.py
+++ modulo-05/funcoes/exercicio02.py
@@ -1,2 +1,15 @@
 # Exercício 2 - Crie uam função que receba uma string como argumento e retorne a mesma string em letras maiúsculas
 # Faça uma chamada à função, passando como parâmetro uma string
+
+def letras_maiusculas(arg):
+  return arg.upper()
+
+letras_maiusculas("argumento")
+
+"""
+def listaString(texto):
+    print(texto.upper())
+    return
+
+listaString('Rumo à Análise de Dados')
+"""

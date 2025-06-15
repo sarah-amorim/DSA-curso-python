f9766d90360cd764147f7390ee6db03e6b1dc311
Sarah Amorim <162258709+sarah-amorim@users.noreply.github.com>
2025-06-15 20:57:09 -0300

    Update exercicio10.py

---
+++ modulo-05/loops-condicionais/exercicio10.py
@@ -0,0 +1,16 @@
+# Exercício 10 - Faça um programa que conte quantas vezes a letra "r" aparece na frase abaixo. Use um placeholder na 
+# sua instrução de impressão
+# “É melhor, muito melhor, contentar-se com a realidade; se ela não é tão brilhante como os sonhos, tem pelo menos a 
+# vantagem de existir.” (Machado de Assis)
+
+contar_r = frase.count("r")
+print(f"A letra 'r' aparece {contar_r} vezes na frase")
+
+"""
+frase = "A gratidão é a virtude das almas nobres. O quão feliz é uma pessoa depende da profundidade de sua gratidão." 
+count = 0
+for caracter in frase:
+    if caracter == 'r':
+        count += 1
+print("O caracter r aparece %s vezes na frase." %(count))
+"""

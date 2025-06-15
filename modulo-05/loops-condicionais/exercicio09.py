d76bf0574c87032255401cf4c0b41f8a9050f4fa
Sarah Amorim <162258709+sarah-amorim@users.noreply.github.com>
2025-06-15 20:48:56 -0300

    Update exercicio09.py

---
+++ modulo-05/loops-condicionais/exercicio09.py
@@ -0,0 +1,14 @@
+# Exercício 9 - Faça a correção dos erros no código abaixo e execute o programa. Dica: são 3 erros.
+"""
+temperatura = float(input('Qual a temperatura? '))
+if temperatura > 30
+print('Vista roupas leves.')
+else
+    print('Busque seus casacos.')
+"""
+
+temperatura = float(input('Qual a temperatura? '))
+if temperatura > 30: 
+  print('Vista roupas leves.')
+else:
+    print('Busque seus casacos.')

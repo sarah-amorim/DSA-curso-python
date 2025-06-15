f275e9b55d14015981b33f502da318086b6343a2
Sarah Amorim <162258709+sarah-amorim@users.noreply.github.com>
2025-06-15 19:25:24 -0300

    Update exercicio10.py

--- modulo-05/funcoes/exercicio10.py
+++ modulo-05/funcoes/exercicio10.py
@@ -1,1 +1,8 @@
 # Exercício 10 - Crie uma list comprehension que imprima os números menores que 5 em um intervalo de 1 a 10
+
+[x for x in range(1, 11) if x < 5]
+
+"""
+resultado = [x for x in range(11) if x < 5]
+print(resultado)
+"""

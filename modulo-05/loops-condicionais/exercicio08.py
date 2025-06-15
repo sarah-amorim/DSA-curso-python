c8374f51e17b24576d830df2ce05e9f13e0978a5
Sarah Amorim <162258709+sarah-amorim@users.noreply.github.com>
2025-06-15 20:45:58 -0300

    Update exercicio08.py

---
+++ modulo-05/loops-condicionais/exercicio08.py
@@ -0,0 +1,9 @@
+# Exercício 8 - Transforme o resultado desta função range em uma lista: range(5, 45, 2)
+
+lista = [x for x in range(5, 45, 2)]
+type(lista)
+
+"""
+nums = range(5, 45, 2)
+print(list(nums))
+"""

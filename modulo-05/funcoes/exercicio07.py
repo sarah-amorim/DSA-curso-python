065f06719dd01d8332276487360845a90cc4d3bd
Sarah Amorim <162258709+sarah-amorim@users.noreply.github.com>
2025-06-15 19:13:02 -0300

    Update exercicio07.py

--- modulo-05/funcoes/exercicio07.py
+++ modulo-05/funcoes/exercicio07.py
@@ -3,6 +3,18 @@
 # Dica: para conseguir realizar este exercício, você deve criar sua função lambda, dentro de uma função
 # (que será estudada no próximo capítulo). Isso permite aplicar sua função a cada elemento da lista
 
+"""
 Celsius = [39.2, 36.5, 37.3, 37.8]
 Fahrenheit = map(coloque_aqui_sua_função_lambda)
 print (list(Fahrenheit))
+"""
+
+Celsius = [39.2, 36.5, 37.3, 37.8]
+Fahrenheit = map(lambda c: (c * 1.8) + 32, Celsius)
+print (list(Fahrenheit))
+
+"""
+Celsius = [39.2, 36.5, 37.3, 37.8]
+Fahrenheit = map(lambda x: (float(9)/5)*x + 32, Celsius)
+print (list(Fahrenheit))
+"""

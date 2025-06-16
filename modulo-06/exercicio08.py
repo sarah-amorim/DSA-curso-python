c9ae1168bc72f5863229ea69c57871ca86fa0912
Sarah Amorim <162258709+sarah-amorim@users.noreply.github.com>
2025-06-15 22:09:11 -0300

    Update exercicio08.py

---
+++ modulo-06/exercicio08.py
@@ -0,0 +1,4 @@
+# Exercício 8 - Considere os dois dicionários abaixo. 
+# Crie um terceiro dicionário com as chaves do dicionário 1 e os valores do dicionário 2.
+dict1 = {'a':1,'b':2}
+dict2 = {'c':4,'d':5}

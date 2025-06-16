97d472982a4730f7c5aa64bad2a670bc689dd41a
Sarah Amorim <162258709+sarah-amorim@users.noreply.github.com>
2025-06-15 21:42:53 -0300

    Update exercicio03.py

---
+++ modulo-06/exercicio03.py
@@ -0,0 +1,7 @@
+# Exercício 3 - Calcule a matriz transposta da matriz abaixo.
+# Caso não saiba o que é matriz transposta, visite este link: https://pt.wikipedia.org/wiki/Matriz_transposta
+# Matriz transposta é um conceito fundamental na construção de redes neurais artificiais, base de sistemas de IA.
+"""
+A matriz transposta é uma matriz resultante da troca das linhas por colunas de uma matriz original. Se a matriz original tem dimensão m x n, a matriz transposta terá dimensão n x m. 
+"""
+matrix = [[1, 2],[3,4],[5,6],[7,8]]

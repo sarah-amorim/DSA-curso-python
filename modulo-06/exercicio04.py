295f97f876d734811c0507769aaf6796b0440c63
Sarah Amorim <162258709+sarah-amorim@users.noreply.github.com>
2025-06-15 21:52:19 -0300

    Update exercicio04.py

---
+++ modulo-06/exercicio04.py
@@ -0,0 +1,4 @@
+# Exercício 4 - Crie duas funções, uma para elevar um número ao quadrado e outra para elevar ao cubo. 
+# Aplique as duas funções aos elementos da lista abaixo. 
+# Obs: as duas funções devem ser aplicadas simultaneamente.
+lista = [0, 1, 2, 3, 4]

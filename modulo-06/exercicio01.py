9a1b5bdefaecf664497fb71f3c04a1b77e6a67ac
Sarah Amorim <162258709+sarah-amorim@users.noreply.github.com>
2025-06-15 21:30:51 -0300

    Update exercicio01.py

---
+++ modulo-06/exercicio01.py
@@ -0,0 +1,10 @@
+# Exercício 1 - Crie uma lista de 3 elementos e calcule a terceira potência de cada elemento.
+
+numeros = [x**3 for x in range(2, 5)]
+print(numeros)
+
+"""
+list1 = [3,4,5]
+quadrado = [item**3 for item in list1] 
+print(quadrado)
+"""

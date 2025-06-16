ba098c54675c0ee3af17c12791b0f2bccd106448
sarah <sarahamorim.050@gmail.com>
2025-06-16 20:33:59 -0300

    Adiciona pasta POO

---
+++ modulo-08/POO/exercicio03.py
@@ -0,0 +1,2 @@
+# Exercício 3 - Crie a classe Smartphone com 2 atributos, tamanho e interface e crie a classe MP3Player com os 
+# atributos capacidade. A classe MP3player deve herdar os atributos da classe Smartphone.

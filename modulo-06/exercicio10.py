b7c4d71b041f5ad854258a12d13de12974314e23
Sarah Amorim <162258709+sarah-amorim@users.noreply.github.com>
2025-06-15 22:20:09 -0300

    Update exercicio10.py

---
+++ modulo-06/exercicio10.py
@@ -0,0 +1,4 @@
+# Exercício 10 - Crie um regex em Python para extrair a palavra que aparece depois das palavras 
+# Data e Science na frase: 'A Data Science Academy oferce os melhores cursos de análise de dados do Brasil.'
+import re
+texto = 'A Data Science Academy oferece os melhores cursos de análise de dados do Brasil.'

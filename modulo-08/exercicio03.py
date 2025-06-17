61287bac5c5a707bde0fb652fa6f97fcbc3505d6
Sarah Amorim <162258709+sarah-amorim@users.noreply.github.com>
2025-06-16 21:49:59 -0300

    Update exercicio03.py

--- modulo-08/exercicio03.py
+++ modulo-08/exercicio03.py
@@ -11,4 +11,20 @@ class MP3Player(Smartphone):
     Smartphone.__init__(self)
     self.capacidade = capacidade
     
-  
+"""
+class Smartphone(object):
+    def __init__(self, tamanho, interface):
+        self.tamanho = tamanho
+        self.interface = interface
+        
+class MP3Player(Smartphone):
+    def __init__(self, capacidade, tamanho = 'Pequeno', interface = 'Led'):
+        self.capacidade = capacidade
+        Smartphone.__init__(self, tamanho, interface)
+        
+    def print_mp3player(self):
+        print("Valores para o objeto criado: %s %s %s"  %(self.tamanho, self.interface, self.capacidade))
+    
+device1 = MP3Player('64 GB')
+device1.print_mp3player()
+"""
